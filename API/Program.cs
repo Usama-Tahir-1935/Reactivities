98595b4ce8073f66d085a31c06244fe14efab050
Malik Usama <malikusama1935@gmail.com>
2024-10-28 00:46:52 +0500

    End of section 12

--- API/Program.cs
+++ API/Program.cs
@@ -2,12 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.AspNetCore.Identity;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => {
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -24,6 +33,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers(); // By using this you enable a flexible and straightforward way to handle routing in ASP.NET Core applications.Top-level route registration for attribute-routed controllers.
 
@@ -55,8 +65,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync(); 
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,9 +1,10 @@
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext // DbContext is the central part of the EF that acts as a bridge b/w ur application and the database.
+    public class DataContext : IdentityDbContext<AppUser> // DbContext is the central part of the EF that acts as a bridge b/w ur application and the database.
     {
         public DataContext(DbContextOptions options) : base(options)
         {
